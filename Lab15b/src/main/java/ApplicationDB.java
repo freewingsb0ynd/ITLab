37045bb4896b9b668937681f4e7bfd401fbcc647
hoangelato <lhp.huyhoangnguyen@gmail.com>
2019-04-18 15:41:13 +0700

    lab15b

--- Lab15b/src/main/java/ApplicationDB.java
+++ Lab15b/src/main/java/ApplicationDB.java
@@ -39,10 +39,7 @@ public class ApplicationDB{
             ResultSet rs = selectAllStudents(conn);
             StringBuilder stringBuilder = new StringBuilder();
             while (rs.next()) {
-                System.out.println(rs.getInt(1) + "  " + rs.getString(2)
-                        + "  " + rs.getString(3));
-
-                stringBuilder.append(String.format("%s\n", rs.getString(2)));
+                stringBuilder.append(String.format("%s \n", rs.getString(2)));
             }
             writeFile("./src/main/java/inputDB.txt", stringBuilder);
 
